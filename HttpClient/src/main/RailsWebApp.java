3235f93e0aec6ee7de2646193889e83bcea2170f
Tony Wang <tonydqwang@gmail.com>
2014-01-07 21:50:26 -0500

    RailsWebApp schedule(view) restriction(full CRUD) prototype tested

--- HttpClient/src/main/RailsWebApp.java
+++ HttpClient/src/main/RailsWebApp.java
@@ -24,43 +24,80 @@ public class RailsWebApp {
 	public static void main(String[] args) {
 		
 		//HttpPost post = new HttpPost("http://localhost:8080/AlgorithmServer/AlgorithmServlet");
-		HttpPost post = new HttpPost("http://elec49x.cloudapp.net/sessions");
+		//HttpPost post = new HttpPost("http://elec49x.cloudapp.net/sessions");
+		//HttpPost post = new HttpPost("http://localhost:3000/sessions");
 		//StringEntity msgEntity;
 		try {
 			//msgEntity = new StringEntity("test string, plz survive");
 			//post.setEntity(msgEntity);
 			
 			List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-			nvps.add(new BasicNameValuePair("authenticity_token","CNo3TfHI5GzfT3j+AEbREXO5LrZhHeFy3X64JVa07z8="));
+
+			//sign in
+			//HttpPost post = new HttpPost("http://localhost:3000/droid-signin");
+			/*nvps.add(new BasicNameValuePair("authenticity_token","CNo3TfHI5GzfT3j+AEbREXO5LrZhHeFy3X64JVa07z8="));
 			nvps.add(new BasicNameValuePair("session[email]","user1@49x.com"));
 			nvps.add(new BasicNameValuePair("session[password]","face2book@"));
-			nvps.add(new BasicNameValuePair("commit","Sign In"));
-			//nvps.add(new BasicNameValuePair("session[password]","face2book@"));
-			//nvps.add(new BasicNameValuePair("email","user1@49x.com"));
-			//nvps.add(new BasicNameValuePair("password","face2book@"));
+			nvps.add(new BasicNameValuePair("commit","Sign In"));*/
+			
+			//view schedule
+			HttpPost post = new HttpPost("http://localhost:3000/droid/schedule");
+			nvps.add(new BasicNameValuePair("key","CNo3TfHI5GzfT3j+AEbREXO5LrZhHeFy3X64JVa07z8="));
+			nvps.add(new BasicNameValuePair("cmd","view"));
+			nvps.add(new BasicNameValuePair("userid","3"));
+			nvps.add(new BasicNameValuePair("start","2013-12-01"));
+			nvps.add(new BasicNameValuePair("end","2014-12-31"));
+			
+			//view restrictions
+			/*HttpPost post = new HttpPost("http://localhost:3000/droid/restriction");
+			nvps.add(new BasicNameValuePair("key","CNo3TfHI5GzfT3j+AEbREXO5LrZhHeFy3X64JVa07z8="));
+			nvps.add(new BasicNameValuePair("cmd","view"));
+			nvps.add(new BasicNameValuePair("userid","2"));	
+			nvps.add(new BasicNameValuePair("start","2013-12-01"));
+			nvps.add(new BasicNameValuePair("end","2014-01-01")); */
+			
+			//add restriction
+			/*HttpPost post = new HttpPost("http://localhost:3000/droid/restriction");
+			nvps.add(new BasicNameValuePair("key","CNo3TfHI5GzfT3j+AEbREXO5LrZhHeFy3X64JVa07z8="));
+			nvps.add(new BasicNameValuePair("cmd","add"));
+			nvps.add(new BasicNameValuePair("userid","3"));	
+			nvps.add(new BasicNameValuePair("start","2013-12-01"));
+			nvps.add(new BasicNameValuePair("end","2014-01-01"));
+			nvps.add(new BasicNameValuePair("reason","V"));
+			nvps.add(new BasicNameValuePair("approve","true"));*/
+			//if failed, return message will not be empty
+			
+			//delete restriction
+			/*HttpPost post = new HttpPost("http://localhost:3000/droid/restriction");
+			nvps.add(new BasicNameValuePair("key","CNo3TfHI5GzfT3j+AEbREXO5LrZhHeFy3X64JVa07z8="));
+			nvps.add(new BasicNameValuePair("cmd","delete"));
+			nvps.add(new BasicNameValuePair("id","32")); */	
+			//if failed, return message will not be empty
+			
+			//update restriction
+			/*HttpPost post = new HttpPost("http://localhost:3000/droid/restriction");
+			nvps.add(new BasicNameValuePair("key","CNo3TfHI5GzfT3j+AEbREXO5LrZhHeFy3X64JVa07z8="));
+			nvps.add(new BasicNameValuePair("cmd","update"));
+			nvps.add(new BasicNameValuePair("id","1"));
+			nvps.add(new BasicNameValuePair("userid","NO_CHANGE"));	
+			nvps.add(new BasicNameValuePair("start","2013-12-01"));
+			nvps.add(new BasicNameValuePair("end","NO_CHANGE"));
+			nvps.add(new BasicNameValuePair("reason","NO_CHANGE"));
+			nvps.add(new BasicNameValuePair("approve","NO_CHANGE"));*/
+			
+			
+			
+			
 			post.setEntity(new UrlEncodedFormEntity(nvps, Consts.UTF_8));
 			
-			/* test passed 2013-09-10
-			BufferedReader roundabout = new BufferedReader(
-					new InputStreamReader(post.getEntity().getContent()));
-			while (roundabout.ready()) {
-				System.out.println(roundabout.readLine());
-			}*/
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		try {
 			HttpClient client = new DefaultHttpClient();
 			((AbstractHttpClient) client).setRedirectStrategy(new LaxRedirectStrategy());
 			HttpResponse response = client.execute(post);
 			ResponseHandler<String> handler = new BasicResponseHandler();
 			String body = handler.handleResponse(response);
 			System.out.println(body);	
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
+			
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
